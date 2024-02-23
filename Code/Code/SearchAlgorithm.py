9b6dc734974dffeee9e9bcd770a688994e4fa424
nedalmartinezb@gmail.com <nedalmartinezb@gmail.com>
2024-02-23 18:34:56 +0100

    Initial commit

---
+++ Code/Code/SearchAlgorithm.py
@@ -0,0 +1,239 @@
+# This file contains all the required routines to make an A* search algorithm.
+#
+__author__ = 'TO_BE_FILLED'
+# _________________________________________________________________________________________
+# Intel.ligencia Artificial
+# Curs 2023 - 2024
+# Universitat Autonoma de Barcelona
+# _______________________________________________________________________________________
+
+from SubwayMap import *
+from utils import *
+import os
+import math
+import copy
+
+
+def expand(path, map):
+    """
+     It expands a SINGLE station and returns the list of class Path.
+     Format of the parameter is:
+        Args:
+            path (object of Path class): Specific path to be expanded
+            map (object of Map class):: All the information needed to expand the node
+        Returns:
+            path_list (list): List of paths that are connected to the given path.
+    """
+    pass
+
+
+def remove_cycles(path_list):
+    """
+     It removes from path_list the set of paths that include some cycles in their path.
+     Format of the parameter is:
+        Args:
+            path_list (LIST of Path Class): Expanded paths
+        Returns:
+            path_list (list): Expanded paths without cycles.
+    """
+    pass
+
+
+def insert_depth_first_search(expand_paths, list_of_path):
+    """
+     expand_paths is inserted to the list_of_path according to DEPTH FIRST SEARCH algorithm
+     Format of the parameter is:
+        Args:
+            expand_paths (LIST of Path Class): Expanded paths
+            list_of_path (LIST of Path Class): The paths to be visited
+        Returns:
+            list_of_path (LIST of Path Class): List of Paths where Expanded Path is inserted
+    """
+    pass
+
+
+def depth_first_search(origin_id, destination_id, map):
+    """
+     Depth First Search algorithm
+     Format of the parameter is:
+        Args:
+            origin_id (int): Starting station id
+            destination_id (int): Final station id
+            map (object of Map class): All the map information
+        Returns:
+            list_of_path[0] (Path Class): the route that goes from origin_id to destination_id
+    """
+    pass
+
+
+def insert_breadth_first_search(expand_paths, list_of_path):
+    """
+        expand_paths is inserted to the list_of_path according to BREADTH FIRST SEARCH algorithm
+        Format of the parameter is:
+           Args:
+               expand_paths (LIST of Path Class): Expanded paths
+               list_of_path (LIST of Path Class): The paths to be visited
+           Returns:
+               list_of_path (LIST of Path Class): List of Paths where Expanded Path is inserted
+    """
+    pass
+
+
+def breadth_first_search(origin_id, destination_id, map):
+    """
+     Breadth First Search algorithm
+     Format of the parameter is:
+        Args:
+            origin_id (int): Starting station id
+            destination_id (int): Final station id
+            map (object of Map class): All the map information
+        Returns:
+            list_of_path[0] (Path Class): The route that goes from origin_id to destination_id
+    """
+    pass
+
+
+def calculate_cost(expand_paths, map, type_preference=0):
+    """
+         Calculate the cost according to type preference
+         Format of the parameter is:
+            Args:
+                expand_paths (LIST of Paths Class): Expanded paths
+                map (object of Map class): All the map information
+                type_preference: INTEGER Value to indicate the preference selected:
+                                0 - Adjacency
+                                1 - minimum Time
+                                2 - minimum Distance
+                                3 - minimum Transfers
+            Returns:
+                expand_paths (LIST of Paths): Expanded path with updated cost
+    """
+    pass
+
+
+def insert_cost(expand_paths, list_of_path):
+    """
+        expand_paths is inserted to the list_of_path according to COST VALUE
+        Format of the parameter is:
+           Args:
+               expand_paths (LIST of Path Class): Expanded paths
+               list_of_path (LIST of Path Class): The paths to be visited
+           Returns:
+               list_of_path (LIST of Path Class): List of Paths where expanded_path is inserted according to cost
+    """
+    pass
+
+
+def uniform_cost_search(origin_id, destination_id, map, type_preference=0):
+    """
+     Uniform Cost Search algorithm
+     Format of the parameter is:
+        Args:
+            origin_id (int): Starting station id
+            destination_id (int): Final station id
+            map (object of Map class): All the map information
+            type_preference: INTEGER Value to indicate the preference selected:
+                            0 - Adjacency
+                            1 - minimum Time
+                            2 - minimum Distance
+                            3 - minimum Transfers
+        Returns:
+            list_of_path[0] (Path Class): The route that goes from origin_id to destination_id
+    """
+    pass
+
+
+def calculate_heuristics(expand_paths, map, destination_id, type_preference=0):
+    """
+     Calculate and UPDATE the heuristics of a path according to type preference
+     WARNING: In calculate_cost, we didn't update the cost of the path inside the function
+              for the reasons which will be clear when you code Astar (HINT: check remove_redundant_paths() function).
+     Format of the parameter is:
+        Args:
+            expand_paths (LIST of Path Class): Expanded paths
+            map (object of Map class): All the map information
+            destination_id (int): Final station id
+            type_preference: INTEGER Value to indicate the preference selected:
+                            0 - Adjacency
+                            1 - minimum Time
+                            2 - minimum Distance
+                            3 - minimum Transfers
+        Returns:
+            expand_paths (LIST of Path Class): Expanded paths with updated heuristics
+    """
+    pass
+
+
+def update_f(expand_paths):
+    """
+      Update the f of a path
+      Format of the parameter is:
+         Args:
+             expand_paths (LIST of Path Class): Expanded paths
+         Returns:
+             expand_paths (LIST of Path Class): Expanded paths with updated costs
+    """
+    pass
+
+
+def remove_redundant_paths(expand_paths, list_of_path, visited_stations_cost):
+    """
+      It removes the Redundant Paths. They are not optimal solution!
+      If a station is visited and have a lower g-cost at this moment, we should remove this path.
+      Format of the parameter is:
+         Args:
+             expand_paths (LIST of Path Class): Expanded paths
+             list_of_path (LIST of Path Class): All the paths to be expanded
+             visited_stations_cost (dict): All visited stations cost
+         Returns:
+             new_paths (LIST of Path Class): Expanded paths without redundant paths
+             list_of_path (LIST of Path Class): list_of_path without redundant paths
+             visited_stations_cost (dict): Updated visited stations cost
+    """
+    pass
+
+
+def insert_cost_f(expand_paths, list_of_path):
+    """
+        expand_paths is inserted to the list_of_path according to f VALUE
+        Format of the parameter is:
+           Args:
+               expand_paths (LIST of Path Class): Expanded paths
+               list_of_path (LIST of Path Class): The paths to be visited
+           Returns:
+               list_of_path (LIST of Path Class): List of Paths where expanded_path is inserted according to f
+    """
+    pass
+
+
+def distance_to_stations(coord, map):
+    """
+        From coordinates, it computes the distance to all stations in map.
+        Format of the parameter is:
+        Args:
+            coord (list):  Two REAL values, which refer to the coordinates of a point in the city.
+            map (object of Map class): All the map information
+        Returns:
+            (dict): Dictionary containing as keys, all the Indexes of all the stations in the map, and as values, the
+            distance between each station and the coord point
+    """
+    pass
+
+
+def Astar(origin_id, destination_id, map, type_preference=0):
+    """
+     A* Search algorithm
+     Format of the parameter is:
+        Args:
+            origin_id (int): Starting station id
+            destination_id (int): Final station id
+            map (object of Map class): All the map information
+            type_preference: INTEGER Value to indicate the preference selected:
+                            0 - Adjacency
+                            1 - minimum Time
+                            2 - minimum Distance
+                            3 - minimum Transfers
+        Returns:
+            list_of_path[0] (Path Class): The route that goes from origin_id to destination_id
+    """
+    pass

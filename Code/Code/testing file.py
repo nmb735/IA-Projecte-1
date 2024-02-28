fc96abe43347a245e58de99d01b717489ebee7d9
nedalmartinezb@gmail.com <nedalmartinezb@gmail.com>
2024-02-28 13:21:46 +0100

    Updates testing according to SS class

--- Code/Code/testing file.py
+++ Code/Code/testing file.py
@@ -11,11 +11,9 @@ if __name__=="__main__":
     infoVelocity_clean = read_information(os.path.join(ROOT_FOLDER, 'InfoVelocity.txt'))
     map.add_velocity(infoVelocity_clean)
 
-
-
     ###BELOW HERE YOU CAN CALL ANY FUNCTION THAT YOU HAVE PROGRAMED TO ANSWER THE QUESTIONS FOR THE TEST###
-
-    #example
+    #example - Used testing
+    """"
     example_path = expand(Path([14, 13, 8, 12]), map)
     example_path = remove_cycles(example_path)
     for path in example_path:
@@ -44,5 +42,105 @@ if __name__=="__main__":
     for k, v in distances.items():
         print(f"{k}:{v}")
     print(round(distances[9], 6))
+"""
 
+    """"
+    Test 1
+    example_path = expand(Path([2]), map)
+    print_list_of_path(example_path)
+    Route: [2, 1]
+    Route: [2, 3]
+    Route: [2, 5]
+    Route: [2, 10]
+    """
+
+    """"
+    Test 2
+    example_path = expand(Path([4,5]), map)
+    print_list_of_path(example_path)
+    Route: [4, 5, 2]
+    Route: [4, 5, 4]
+    Route: [4, 5, 6]
+    Route: [4, 5, 10]
+    """
+
+    """""
+    # Test 3
+    example_path = expand(Path([4,5,6]), map)
+    print_list_of_path(example_path)
+    print("----------")
+    example_path = remove_cycles(example_path)
+    print_list_of_path(example_path)
+    Route: [4, 5, 6, 5]
+    Route: [4, 5, 6, 7]
+    ----------
+    Route: [4, 5, 6, 7]
+    """
+
+    """"
+    # Test 4
+    example_path = remove_cycles([Path([7,7])])
+    print_list_of_path(example_path)
+
+    (retorna buit)
+    """
+
+    """"
+    # Test 5
+    example_path = depth_first_search(3, 10, map)
+    print_list_of_path([example_path])
+    Route: [3, 2, 5, 6, 7, 8, 12, 11, 10]
+    """
+
+    """
+    # Test 6
+    example_path = depth_first_search( 10 , 10, map )
+    print_list_of_path([example_path])
+    Route: [10]
+    """
+
+    """""
+    #Test 7
+    example_path = depth_first_search( 1 , 10, map )
+    print_list_of_path([example_path])
+    print("--------------------")
+    example_path = breadth_first_search( 1 , 10, map )
+    print_list_of_path([example_path])
+    Route: [1, 2, 5, 6, 7, 8, 12, 11, 10]
+    --------------------
+    Route: [1, 2, 10]
+    """
+
+    """"
+    # Test 8 --> ERROR. Ha de retornar llista buida
+    example_path = breadth_first_search( 10, 10000, map)
+    print_list_of_path([example_path])
+    ERROR
+    """
+
+    """
+    # Test 9
+    distances = distance_to_stations([0, 0], map)
+    for k, v in distances.items():
+        print(f"{k}:{v}")
+    1:103.58571330062848
+    4:142.57980221616245
+    2:150.78461459976612
+    5:150.78461459976612
+    10:150.78461459976612
+    11:172.10461934532728
+    3:178.84350701101786
+    6:181.1767093199344
+    7:210.35446275275456
+    9:231.97629189208106
+    8:232.59406699226014
+    12:232.59406699226014
+    13:232.59406699226014
+    14:275.68822970885066
+    """
+
+    
+
+
+    
 

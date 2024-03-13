7ca8e354fbba9cefb6a0c76b3da1654fdd79a7fc
nedalmartinezb@gmail.com <nedalmartinezb@gmail.com>
2024-03-13 01:56:36 +0100

    Add test functions and main test function

--- Code/Code/testing-file.py
+++ Code/Code/testing-file.py
@@ -6,6 +6,109 @@ def print_list_of_path_with_heu(path_list):
     for p in path_list:
         print("Route: {}, \t Cost: {}".format(p.route, round(p.h,2)))
 
+def test_1(n):
+    print(f"He programat i provat {n} funcions.")
+
+def test_2():
+    example_path = calculate_cost(expand(Path([2]), map), map, 3)
+    print_list_of_path_with_heu(example_path)
+
+def test_3():
+    example_path = expand(Path([5]), map)
+    calculate_cost(example_path, map,2)
+    print_list_of_path_with_cost(example_path)
+
+def test_4():
+    path = uniform_cost_search(10, 14, map, 0)
+    print_list_of_path_with_cost([path])
+
+def test_5():
+    path = uniform_cost_search(10, 10, map, 2)
+    print_list_of_path_with_cost([path])
+
+def test_6():
+    print("No llegible")
+
+def test_7(x,y):
+    d = distance_to_stations([x,y], map)
+    for s, x in d.items():
+        print(f"Station: {s}; Distance: {x}.")
+
+def test_8():
+    x = Astar(4,14,map,1)
+    print_list_of_path_with_cost([x])
+
+def test_9(x):
+    print(f"Espabila crack, la has liado en {x} funciones (como minimo).")
+
+def test_main():
+    print("INITIALIZING TESTS")
+    print("#"*50)
+    i = 0
+    print("="*25)
+    print("Test 1")
+    print("="*25)
+    t1 = int(input("How many functions have you programmed and tested? "))
+    test_1(t1)
+    t1 = input("La has liado? (y/n) ")
+    if t1 == "y":
+        i += 1
+    print("#"*50)
+    print("="*25)
+    print("Test 2")
+    print("="*25)
+    test_2()
+    t2 = input("La has liado? (y/n) ")
+    if t2 == "y":
+        i += 1
+    print("#"*50)
+    print("="*25)
+    print("Test 3")
+    print("="*25)
+    test_3()
+    t3 = input("La has liado? (y/n) ")
+    if t3 == "y":
+        i += 1
+    print("#"*50)
+    print("="*25)
+    print("Test 4")
+    print("="*25)
+    test_4()
+    t4 = input("La has liado? (y/n) ")
+    if t4 == "y":
+        i += 1
+    print("#"*50)
+    print("="*25)
+    print("Test 5")
+    print("="*25)
+    test_5()
+    t5 = input("La has liado? (y/n) ")
+    if t5 == "y":
+        i += 1
+    print("#"*50)
+    print("="*25)
+    print("Test 6")
+    print("="*25)
+    test_6()
+    t6 = input("La has liado? (y/n) ")
+    if t6 == "y":
+        i += 1
+    print("#"*50)
+    print("="*25)
+    print("Test 7")
+    print("="*25)
+    x = int(input("X Coordinate: "))
+    y = int(input("Y Coordinate: "))
+    test_7(x,y)
+    t7 = input("La has liado? (y/n) ")
+    if t7 == "y":
+        i += 1
+    print("#"*50)
+    print("="*25)
+    print("Test 8")
+    print("="*25)
+    test_8(i)
+
 if __name__=="__main__":
     ROOT_FOLDER = '../CityInformation/Lyon_SmallCity/'
     map = read_station_information(os.path.join(ROOT_FOLDER, 'Stations.txt'))
@@ -17,24 +120,6 @@ if __name__=="__main__":
 
     #example_path = expand(Path([5]), map)
     #print_list_of_path_with_cost(example_path)
+    test_main()
 
-
-    route = uniform_cost_search(9, 3, map, 0)
-    print_list_of_path_with_cost([route])
-    print_list_of_path_with_cost(calculate_cost([Path([9, 8, 7, 6, 5, 2, 3])], map, 0))
-    print("#"*50)
-
-    route = uniform_cost_search(9, 3, map, 1)
-    print_list_of_path_with_cost([route])
-    print_list_of_path_with_cost(calculate_cost([Path([9, 8, 12, 11, 10, 2, 3])], map, 1))
-    print("#"*50)
-
-    route = uniform_cost_search(9, 3, map, 2)
-    print_list_of_path_with_cost([route])
-    print_list_of_path_with_cost(calculate_cost([Path([9, 8, 12, 11, 10, 2, 3])], map, 2))
-    print("#"*50)
-
-    route = uniform_cost_search(9, 3, map, 3)
-    print_list_of_path_with_cost([route])
-    print_list_of_path_with_cost(calculate_cost([Path([9, 8, 7, 6, 5, 2, 3])], map, 3))
-    print("#"*50)
+    
